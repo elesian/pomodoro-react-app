61fcef27c30b2e9cd9c76ce8acea3b3784ff80e2
elesian <genovacle@hotmail.com>
2022-01-30 21:56:52 +0000

    Timer added

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,10 @@
 /** @format */
 
-import Pomodoro from './';
-
 const Header = () => {
   return (
     <header className="Header">
-      <h1>
-        Basic React Pomodoro Timer
-        <img src="{Pomodoro}" className="header-image" alt="Pomodoro"></img>
-      </h1>
+      <h1>Pomodoro Timer </h1>
+      <img src="Pomodoro.jpg" className="header-image" alt="Pomodoro" />
     </header>
   );
 };
--- src/Timer.js
+++ src/Timer.js
@@ -1,7 +1,79 @@
 /** @format */
 
+import React from 'react';
+import { useState, useEffect } from 'react';
+import styles from './App.css';
+
 const Timer = (props) => {
-  return <h1>Timer</h1>;
+  const { initialMinute = 0, initialSeconds = 2 } = props;
+  const [minutes, setMinutes] = useState(initialMinute);
+  const [seconds, setSeconds] = useState(initialSeconds);
+  const [phase, setPhase] = useState(1);
+  const [status, setStatus] = useState('WORK');
+  const [rounds, setRounds] = useState(1);
+  useEffect(() => {
+    let myInterval = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      }
+      if (seconds === 0) {
+        if (minutes === 0) {
+          clearInterval(myInterval);
+        } else {
+          setMinutes(minutes - 1);
+          setSeconds(59);
+        }
+      }
+    }, 1000);
+    return () => {
+      clearInterval(myInterval);
+    };
+  });
+
+  const resetPhases = () => {
+    if (minutes === 0 && seconds === 0) {
+      if (phase % 2 === 0 && phase < 8) {
+        setMinutes(0);
+        setSeconds(2);
+        setStatus('WORK');
+        setPhase(phase + 1);
+        setRounds(rounds + 1);
+      }
+      if (phase % 2 === 1 && phase <= 9) {
+        if (phase === 9) {
+          setStatus('WORK');
+          setPhase(2);
+          setRounds(0);
+        } else {
+          setMinutes(0);
+          setSeconds(2);
+          setPhase(phase + 1);
+          setStatus('REST');
+        }
+      }
+      if (phase === 8) {
+        setStatus('EXTENDED REST');
+        setMinutes(0);
+        setSeconds(2);
+        setPhase(phase + 1);
+      }
+
+      return;
+    }
+    return;
+  };
+
+  return (
+    <div className="Timer">
+      <h1>Current Task : {}</h1>
+      {resetPhases()}
+      <h1>Completed Rounds: {rounds}/4</h1>
+      <h1>{`${status} FOR:`} </h1>
+      <h1 className="Counter">
+        {minutes} : {seconds}{' '}
+      </h1>
+    </div>
+  );
 };
 
 export default Timer;
